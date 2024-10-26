bff86fe076f98de7461643acb563d3f3a436cbfc
Mahammad <quseynovmaqa25@gmail.com>
2024-10-26 18:05:39 +0400

    toplama

---
+++ main.java
@@ -0,0 +1,15 @@
+public class main {
+    public static void main(String[] args){
+        int number1=15;
+        int number2=7;
+        int num=number2+number1;
+        int a=number1-number2;
+        int b=number1/number2;
+        int c=number1%number2;
+
+        System.out.printf("cavab="+num);
+        System.out.printf("cavab="+a);
+        System.out.printf("cavab="+b);
+        System.out.printf("cavab="+c);
+    }
+}
